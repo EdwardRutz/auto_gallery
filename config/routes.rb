453ca2ec1eb06fdbefca138083fd0fb3e4bc1e1c
Edward R <96d7cf74@opayq.com>
2017-12-13 17:46:18 -0600

    Created acess_controller and updates for authentications

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
   get '/search', to: 'autos#search', as: 'search'
   get '/sales_console', to: 'autos#sales_console', as: 'sales_console'
 
-
   # Users
   get '/dashboard', to: 'users#dashboard', as: 'dashboard'
 
   # Welcome
   get '/contact', to: 'welcome#contact', as: 'contact'
   get '/about', to: 'welcome#about', as: 'about'
+
+  # Access
+  get '/admin', to: 'access#menu', as: 'admin'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
 
   resources :autos
   resources :users
