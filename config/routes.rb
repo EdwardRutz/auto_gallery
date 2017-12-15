5f20b6a312fdd06efc16b134c55a64394edf3bda
Edward R <96d7cf74@opayq.com>
2017-12-14 20:39:03 -0600

    Worked on logout

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :autos
   resources :users
