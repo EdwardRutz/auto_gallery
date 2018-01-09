8b712c71330d24a2d37d178af04a99534bd2ab36
Edward R <96d7cf74@opayq.com>
2018-01-08 20:50:38 -0600

    Changed so logout goes to root

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   # get '/logout', to: 'welcome#index'
 
 
